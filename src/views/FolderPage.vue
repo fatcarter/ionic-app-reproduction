6d1946acb4e7a80c1b152738a1096e577b13df61
michaelzhang055 <michaelzhang055@hotmail.com>
2023-06-25 14:27:29 +0800

    init

--- src/views/FolderPage.vue
+++ src/views/FolderPage.vue
@@ -10,6 +10,9 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
+      <ion-nav-link router-direction="forward" :component="PageTwo" :router-animation="getRouterAnimation">
+        <ion-button>Go to Page Two</ion-button>
+      </ion-nav-link>
       <ion-header collapse="condense">
         <ion-toolbar>
           <ion-title size="large">{{ $route.params.id }}</ion-title>
@@ -25,7 +28,41 @@
 </template>
 
 <script setup lang="ts">
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import {
+  IonNavLink,
+  IonButton,
+  IonButtons,
+  IonContent,
+  IonHeader,
+  IonMenuButton,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  createAnimation
+} from '@ionic/vue';
+import PageTwo from "@/views/PageTwo.vue";
+const getRouterAnimation = (baseEl: any, opts: any) => {
+  console.log("opts", opts);
+  const rootTransition = createAnimation()
+      .addElement(baseEl)
+      .duration(opts.duration || 333);
+
+  const enterTransition = createAnimation().addElement(opts.enteringEl);
+  const exitTransition = createAnimation().addElement(opts.leavingEl);
+
+  enterTransition.fromTo('opacity', '0', '1');
+  exitTransition.fromTo('opacity', '1', '0');
+
+  if (opts.direction === 'forward') {
+    enterTransition.fromTo('transform', 'translateX(-1.5%)', 'translateX(0%)');
+    exitTransition.fromTo('transform', 'translateX(0%)', 'translateX(1.5%)');
+  } else {
+    enterTransition.fromTo('transform', 'translateX(1.5%)', 'translateX(0%)');
+    exitTransition.fromTo('transform', 'translateX(0%)', 'translateX(-1.5%)');
+  }
+  rootTransition.addAnimation([enterTransition, exitTransition]);
+  return rootTransition;
+};
 </script>
 
 <style scoped>
---
+++ src/views/PageTwo.vue
@@ -0,0 +1,20 @@
+<template>
+  <ion-header>
+    <ion-toolbar>
+      <ion-back-button>The Page Two</ion-back-button>
+    </ion-toolbar>
+  </ion-header>
+
+  <ion-nav-link :component="PageThree" router-direction="forward">
+    <ion-button>Go to Page Three</ion-button>
+  </ion-nav-link>
+</template>
+
+<script setup lang="ts">
+import PageThree from "@/views/PageThree.vue";
+import {IonButton, IonNavLink,IonBackButton} from "@ionic/vue";
+</script>
+
+<style scoped>
+
+</style>
